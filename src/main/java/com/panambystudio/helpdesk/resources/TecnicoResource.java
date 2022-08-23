5cc201878a13d6f239d0176f65d5138ef9222eea
Rafael <rafael.souza@prill.com.br>
2022-08-23 13:39:21 -0300

    Endpoint para update de Tecnico

--- src/main/java/com/panambystudio/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/panambystudio/helpdesk/resources/TecnicoResource.java
@@ -1,6 +1,5 @@
 package com.panambystudio.helpdesk.resources;
 
-import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,27 +30,24 @@ public class TecnicoResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id){
 		
-		Tecnico obj = service.findById(id);
-		
-		return ResponseEntity.ok().body(new TecnicoDTO(obj));
+		return ResponseEntity.ok().body(new TecnicoDTO(service.findById(id)));
 	}
 	
 	@GetMapping
 	public ResponseEntity<List<TecnicoDTO>> findAll(){
 		
-		List<Tecnico> list = service.findAll();
-		List<TecnicoDTO> listDTO = list.stream().map(obj -> new TecnicoDTO(obj)).collect(Collectors.toList());
-		
-		return ResponseEntity.ok().body(listDTO);
+		return ResponseEntity.ok().body(service.findAll().stream().map(obj -> new TecnicoDTO(obj)).collect(Collectors.toList()));
 	}
 	
 	@PostMapping
 	public ResponseEntity<TecnicoDTO> create(@Valid @RequestBody TecnicoDTO objDTO){
 		
-		Tecnico newObj = service.create(objDTO);
+		return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(service.create(objDTO).getId()).toUri()).build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<TecnicoDTO> update(@PathVariable Integer id, @Valid @RequestBody TecnicoDTO objDTO){
 		
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
-		
-		return ResponseEntity.created(uri).build();
+		return ResponseEntity.ok().body(new TecnicoDTO(service.update(id, objDTO)));
 	}
 }
